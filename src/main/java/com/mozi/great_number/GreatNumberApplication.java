238d09f55b3888a4f9643fb860bef25300d13453
Mozi <seung5727@naver.com>
2024-12-23 22:43:48 +0900

    feat: 완성본

--- src/main/java/com/mozi/great_number/GreatNumberApplication.java
+++ src/main/java/com/mozi/great_number/GreatNumberApplication.java
@@ -3,11 +3,61 @@ package com.mozi.great_number;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import java.util.Scanner;
+
 public class GreatNumberApplication {
 
+	static final int SIZE_OF_SUBSTRING = 3;
+
 	public static void main(String[] args) {
-		SpringApplication.run(GreatNumberApplication.class, args);
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("숫자를 입력해주세요: ");
+		String input = scanner.nextLine();
+		System.out.println(calculateGreatString(input));
+
+	}
+
+	public static int calculateGreatString(String input) {
+
+		String result = "-1";
+
+		for (int i = 0; i < input.length(); i++) {
+
+			// 숫자 묶음이 3개가 안될 경우
+			if (isPossibleSubstring(input, i)) {
+				break;
+			}
+
+			String subString = input.substring(i, i + SIZE_OF_SUBSTRING);
+
+			// substring이 전부 같은 숫자이고 동시에 기존에 저장된 substring보다 클 경우
+			if (isAllSameNumber(subString) && isMaxNumber(result, subString)) {
+				result = subString;
+			}
+		}
+
+		return Integer.parseInt(result);
+	}
+
+	// 묶음 가능한 substring 판별 메서드
+	public static boolean isPossibleSubstring(String input, int i) {
+		return input.length() - i < SIZE_OF_SUBSTRING;
+	}
+
+	// substring이 전부 같은 숫자인지 판별하는 메서드
+	public static boolean isAllSameNumber(String string) {
+
+		char first = string.charAt(0);
+		for (char c : string.toCharArray()) {
+			if (c != first) return false;
+		}
+		return true;
+	}
+
+	// 두 스트링 중 10진수가 큰 값을 반환하는 메서드
+	public static boolean isMaxNumber(String result, String subString) {
+		return Integer.parseInt(result) < Integer.parseInt(subString);
 	}
 
 }
--- src/test/java/com/mozi/great_number/GreatNumberApplicationTests.java
+++ src/test/java/com/mozi/great_number/GreatNumberApplicationTests.java
@@ -3,11 +3,17 @@ package com.mozi.great_number;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static com.mozi.great_number.GreatNumberApplication.calculateGreatString;
+import static org.assertj.core.api.Assertions.assertThat;
+
 class GreatNumberApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(calculateGreatString("12223")).isEqualTo(222);
+		assertThat(calculateGreatString("111999333")).isEqualTo(999);
+		assertThat(calculateGreatString("123")).isEqualTo(-1);
+		assertThat(calculateGreatString("12")).isEqualTo(-1);
 	}
 
 }
