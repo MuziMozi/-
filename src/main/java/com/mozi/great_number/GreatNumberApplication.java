3876a08de9283664e08a4bc918343cbb6043eb03
Mozi <seung5727@naver.com>
2024-12-23 22:12:06 +0900

    초기 커밋

---
+++ src/main/java/com/mozi/great_number/GreatNumberApplication.java
@@ -0,0 +1,13 @@
+package com.mozi.great_number;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatNumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatNumberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mozi/great_number/GreatNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mozi.great_number;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreatNumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
